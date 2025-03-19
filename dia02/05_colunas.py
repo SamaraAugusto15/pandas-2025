2bf4944508e9be808490d288a7a8681c274732b5
Samara Pereira Augusto <samaraaugusto1511@hotmail.com>
2025-03-19 15:16:25 -0400

    Aula 04 Colunas

---
+++ dia02/05_colunas.py
@@ -0,0 +1,68 @@
+#%%
+
+import pandas as pd
+
+df = pd.read_csv("../data/transacoes.csv")
+df
+
+#%%
+df.shape
+
+#%%
+df.info(memory_usage='deep')
+
+#%%
+df.dtypes
+
+#%%
+renamed_columns = {
+    "qtdePontos":"qtPontos",
+    "descSistemaOrigem": "SistemaOrigem"
+}
+#%%
+df =df.rename(columns=renamed_columns)
+
+#%%
+## OU
+df = df.rename(columns={"qtdePontos":"qtPontos",
+                        "descSistemaOrigem": "SistemaOrigem"})
+
+#%%
+df.rename(columns=renamed_columns, inplace=True)
+
+#%%
+colunas = ["idCliente", "qtPontos"]
+df[colunas]
+
+#%%
+#SELECT * FROM df
+df
+
+#%%
+#SELECT idCliente FROM df
+df[["idCliente"]]
+
+#%%
+#SELECT idCliente, qtPontos FROM df LIMIT 5
+df[["idCliente", "qtPontos"]].head(5)
+
+#%%
+#reordenar colunas
+#SELECT idCliente, idTransacao, qtPontos FROM df LIMIT 5
+
+df[["idCliente", "idTransacao" ,"qtPontos"]].head(5)
+
+#%%
+#ordenar em ordem alfabetica
+colunas_2 = df.columns.tolist()
+
+#%%
+## OU
+colunas_2 = list(df.columns)
+#%%
+colunas_2.sort()
+colunas_2
+
+#%%
+df = df[colunas_2]
+df
