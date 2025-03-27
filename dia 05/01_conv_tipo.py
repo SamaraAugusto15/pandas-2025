75a1aa71f88572103d9acb4dec19af0312a71368
Samara Pereira Augusto <samaraaugusto1511@hotmail.com>
2025-03-27 15:03:47 -0400

    Aula 08 Conversão

--- dia 05/01_conv_tipo.py
+++ dia 05/01_conv_tipo.py
@@ -7,3 +7,11 @@ df = pd.read_csv("../data/clientes.csv")
 #%%
 #para converter .astype
 df["qtdePontos"].astype(float).astype(str)
+
+#%%
+replace = {"0000-00-00 00:00:00.000": "2024-02-01 09:00:00.000"}
+
+df["dtCriacao"] = pd.to_datetime(df["dtCriacao"].replace(replace))
+
+#%%
+df["dtCriacao"].dt.month
