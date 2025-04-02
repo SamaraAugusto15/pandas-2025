a04c77636e5be912749cb86e155a3e7f69f05d81
Samara Pereira Augusto <samaraaugusto1511@hotmail.com>
2025-04-02 13:54:58 -0400

    Aula 11 Apply ex

--- dia 06/02_ufs.py
+++ dia 06/02_ufs.py
@@ -19,9 +19,6 @@ numero = "251 529,2"
 str_to_float(numero)
 
 #%%
-uf.head()
-
-#%%
 uf["Área (km²)"] = uf["Área (km²)"].apply(str_to_float)
 uf["População (Censo 2022)"] = uf["População (Censo 2022)"].apply(str_to_float)
 uf["PIB (2015)"] = uf["PIB (2015)"].apply(str_to_float)
@@ -31,4 +28,31 @@ uf["PIB per capita (R$) (2015)"] = uf["PIB per capita (R$) (2015)"].apply(str_to
 colunas = "Área (km²)", "População (Censo 2022)", "PIB (2015)", "PIB per capita (R$) (2015)"
 
 #%%
-uf.dtypes
+
+def exp_to_anos(exp):
+    return float(exp.replace("," , ".")
+                    .replace(" anos", ""))
+    
+uf["Expectativa de vida (2016)"] = uf["Expectativa de vida (2016)"].apply(exp_to_anos)
+
+#%%
+uf.head()
+
+#%%
+def uf_to_regiao(uf):
+
+    if uf in ["Distrito Federal", "Goiás", "Mato Grosso", "Mato Grosso do Sul"]:
+        return "Centro-Oeste"
+    elif uf in ["Alagoas","Bahia", "Ceará", "Maranhão", "Paraíba", "Pernambuco", "Piauí", "Rio Grande do Norte", "Sergipe"]:
+        return "Nordeste"
+    elif uf in ["Acre", "Amapá", "Amazonas", "Pará", "Rondônia", "Roraima", "Tocantins"]:
+        return "Norte"
+    elif uf in ["Espírito Santo","Minas Gerais", "Rio de Janeiro", "São Paulo"]:
+        return "Sudeste"
+    elif uf in ["Paraná", "Rio Grande do Sul", "Santa Catarina"]:
+        return "Sul"
+    
+uf["Região"] = uf["Unidade federativa"].apply(uf_to_regiao)
+
+#%%
+uf.head()
