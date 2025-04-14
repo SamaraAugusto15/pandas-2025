305d7e98bbc5e881dab26a713c19135e1ec8a237
Samara Pereira Augusto <samaraaugusto1511@hotmail.com>
2025-04-14 15:50:47 -0400

    aula 12 Groupy by

--- dia 07/01_summary.py
+++ dia 07/01_summary.py
@@ -5,15 +5,25 @@ idades = [32,44,15,54,67,32,23,34,14,45,43,28,73,29]
 
 idades = pd.Series(idades)
 idades
-
-# %%
 idades.sum()
-
-# %%
 idades.min()
-
-# %%
 idades.max()
+idades.mean()
+idades.describe()
 
 # %%
-idades.mean()
+clientes = pd.read_csv("../data/clientes.csv")
+clientes
+
+# %%
+clientes["flTwitch"].sum()
+clientes["flTwitch"].mean()
+
+# %%
+redes_sociais = ["flEmail","flTwitch","flYouTube","flBlueSky","flInstagram"]
+clientes[redes_sociais].mean()
+
+# %%
+clientes[redes_sociais].sum()
+
+# %%
