ddbd90d0bf23e74399cce3e21c6024353e358798
Samara Pereira Augusto <samaraaugusto1511@hotmail.com>
2025-05-15 10:33:29 -0400

    merge

--- dia 08/01_merge.py
+++ dia 08/01_merge.py
@@ -16,3 +16,25 @@ transacoes.merge(
     on=['idCliente'],
     suffixes=["Transacao", "Cliente"]
     )
+
+#%%
+
+df_1 = pd.DataFrame({
+    "transacao": [1,2,3,4,5],
+    "nome": ["t1", "t2", "t3", "t4", "t5"],
+    "idCliente": [1,2,3,2,2],
+    "valor": [10,45,32,17,87],
+})
+
+df_2 = pd.DataFrame({
+    "id": [1,2,3,4],
+    "nome": ["teo", "nah", "mah", "jose"],
+})
+
+df_1.merge(
+    df_2,
+    left_on=["idCliente"],
+    right_on=["id"],
+    how='left',
+    suffixes=["Transacao", "Cliente"],
+)
