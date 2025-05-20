bea33dd780b01b58858cd2865f73d6fc30ff8da1
Samara Pereira Augusto <samaraaugusto1511@hotmail.com>
2025-05-20 11:47:49 -0400

    concat

--- exercicio/case/case.py
+++ exercicio/case/case.py
@@ -1,12 +1,24 @@
 #%%
 import pandas as pd
+import os
 
-df_geral = pd.read_csv("../../data/ipea/homicidios.csv", sep=";")
-df_geral = df_geral.rename(columns={"valor":"homicidios"})
-df_geral.head()
+def read_file(file_name:str):
+
+    df = (pd.read_csv(f"../../data/ipea/{file_name}.csv", sep=";")
+            .rename(columns={"valor":file_name})
+            .set_index(["nome", "período"])
+            .drop(["cod"], axis=1))
+    
+    return df
+
+#%%
+file_names = os.listdir("../../data/ipea")
+
+dfs = []
+for i in file_names:
+    file_name = i.split(".")[0]
+    dfs.append(read_file(file_name))
 
 #%%
 
-df_negros = 
-
-
+pd.concat(dfs, axis=1).reset_index().sort_values(["período", "nome"])
